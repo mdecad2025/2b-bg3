be44bc0fcd8a59dbf765bd7a1c1f3c04652a66af
41123250-3 <tzuchieh515@gmail.com>
2025-12-21 15:12:19 +0000

    test

--- cmsimde/flaskapp.py
+++ cmsimde/flaskapp.py
@@ -3301,13 +3301,5 @@ def snTosr(tag):
         return tagStr.replace("\n", "\r")
     else:
         return tagStr
-    
-@app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
-
 if __name__ == "__main__":
     app.run()
